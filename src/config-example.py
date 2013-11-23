9321c94efe51a0b0506ceb893f5209a1ddccc983
Julien <contact@julien-tanguy.com>
2013-11-23 01:14:21 +0100

    Added config example file

---
+++ src/config-example.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+HIPCHAT_TOKEN='token'
+HIPCHAT_ROOMNAME='room_name'
+HIPCHAT_SLEEPDELAY=3 # 100 request for 5 minutes !
+
+OLARK_USERNAME='username@olark.com'
+OLARK_PASSWORD='password'
